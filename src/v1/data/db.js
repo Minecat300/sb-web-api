e3359541f1a45356702a7eef5480d76d8b21244f
snik109 <jonas.flydahl@gmail.com>
2026-05-06 11:49:37 +0200

    fiksa problemer med middleware som fortsatt forventet en cookie og newUserRepository som brukte feil table name

--- src/v1/data/db.js
+++ src/v1/data/db.js
@@ -28,8 +28,6 @@ export async function getPool() {
         const user = process.env.DB_USER;
         const database = process.env.DB_NAME;
 
-        console.log(DB_PASSWORD, host, user, database);
-
         pool = await mysql.createPool({
             host,
             user,
